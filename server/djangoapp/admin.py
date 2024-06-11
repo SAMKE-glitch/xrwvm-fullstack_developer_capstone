65dd94581b4b744f3250380201dfa54d42676df2
SAMKE-glitch <sammythemwa@gmail.com>
2024-06-11 06:48:02 -0400

    models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,7 @@
-# from django.contrib import admin
-# from .models import related models
+#from django.contrib import admin
+#from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -11,3 +13,20 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+# Optional: CarModelInline class to include in CarMakeAdmin
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
+
+# Optional: CarModelAdmin class to customize the CarModel display
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'mileage')
+
+# Optional: CarMakeAdmin class to include CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
+# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,16 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country_of_origin = models.CharField(max_length=100, blank=True, null=True)  # Optional field for country of origin
+    established_year = models.IntegerField(blank=True, null=True)  # Optional field for the year established
+
+    def __str__(self):
+        return self.name
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +33,28 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship with CarMake
+    dealer_id = models.IntegerField(null=True)
+  # Dealer ID referring to a dealer created in Cloudant database
+    name = models.CharField(max_length=100)  # Name of the car model
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')  # Type of the car body
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ],
+        default=2023
+    )  # Year of the car model
+    mileage = models.IntegerField()  # Mileage of the car
+
+    def __str__(self):
+        return self.name
+
