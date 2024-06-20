8e7792b1b1bad8df8949501cbfa763c9e0c0cedf
SAMKE-glitch <sammythemwa@gmail.com>
2024-06-20 06:00:02 -0400

    settings

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -20,15 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
-            'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+key = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+sUrl = 'sammythemwa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ALLOWED_HOSTS = [
     'localhost',
-    'sammythemwa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    sUrl
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://sammythemwa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
