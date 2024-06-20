15438d9c1ff3c5f5a67aef5684632c31d52d137f
SAMKE-glitch <sammythemwa@gmail.com>
2024-06-20 05:49:39 -0400

    admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -7,14 +7,15 @@ from .models import CarMake, CarModel
 # Define CarModelInline class to include in CarMakeAdmin
 class CarModelInline(admin.StackedInline):
     model = CarModel
-    # Display 5 empty forms for adding CarModel 
+    # Display 5 empty forms for adding CarModel
     # instances under each CarMake
     extra = 5
+
 
 # Define CarModelAdmin class to customize the CarModel display
 class CarModelAdmin(admin.ModelAdmin):
     # Include 'mileage' field in list_display
-    list_display = ('name', 'car_make', 'type', 'year', 'mileage')  
+    list_display = ('name', 'car_make', 'type', 'year', 'mileage')
 
 
 # Define CarMakeAdmin class to include CarModelInline
